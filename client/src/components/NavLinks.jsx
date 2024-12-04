fc15ab7dbe2581b4de6e058ee2a94c91003c800d
Tommy Nguyen <tommytimmytony@gmail.com>
2024-12-04 01:11:19 -0600

    Fixed some stuff

---
+++ client/src/components/NavLinks.jsx
@@ -0,0 +1,65 @@
+import { NavLink, useNavigate } from "react-router-dom";
+import { CiLocationOn } from "react-icons/ci";
+import { FaHome } from "react-icons/fa";
+import { BiSolidLogInCircle } from "react-icons/bi";
+import { GiRadarDish } from "react-icons/gi";
+import { useWeather } from "../context/WeatherContext";
+
+export default function NavLinks() {
+  const navigate = useNavigate();
+  const { setCity, user } = useWeather();
+
+  async function findCurLocation() {
+    if ("geolocation" in navigator) {
+      // Get current position
+      console.log("Getting current location...");
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setCity(`${latitude},${longitude}`);
+          setCitySelected("");
+          navigate("/");
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+    } else {
+      alert("Geolocation is not available in this browser.");
+    }
+  }
+  return (
+    <>
+      {" "}
+      {/* Current Location Button */}
+      <NavLink
+        to="/"
+        className="flex items-center px-4 py-2 bg-gray-700 rounded-lg shadow-md hover:bg-gray-600"
+        onClick={() => findCurLocation()}
+      >
+        <CiLocationOn size={25} /> &nbsp; Current Location
+      </NavLink>
+      {/* Home */}
+      <NavLink
+        to="/"
+        className="flex items-center px-4 py-2 bg-gray-700 rounded-lg shadow-md hover:bg-gray-600"
+      >
+        <FaHome size={25} /> &nbsp; Home
+      </NavLink>
+      {/* Radar */}
+      <NavLink
+        to="radar"
+        className="flex items-center px-4 py-2 bg-gray-700 rounded-lg shadow-md hover:bg-gray-600"
+      >
+        <GiRadarDish size={25} /> &nbsp; Radar
+      </NavLink>
+      {/* Login */}
+      <NavLink
+        to="login"
+        className="flex items-center px-4 py-2 bg-gray-700 rounded-lg shadow-md hover:bg-gray-600"
+      >
+        <BiSolidLogInCircle size={25} /> &nbsp; {user ? "Logout" : "Login"}
+      </NavLink>
+    </>
+  );
+}
--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -22,7 +22,6 @@ import {
 } from "../functions.js";
 import WeatherForecast from "../components/WeatherForecast.jsx";
 import NextDayForecast from "../components/NextDayForecast.jsx";
-import { useParams } from "react-router-dom";
 import { useWeather } from "../context/WeatherContext.js";
 
 export default function Dashboard() {
